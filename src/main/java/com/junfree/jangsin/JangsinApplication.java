f28bf6bb6544390845549fad0a96f82da664eb15
JunFreeCode <seojun03@gmail.com>
2022-10-10 20:55:42 +0900

    init

---
+++ src/main/java/com/junfree/jangsin/JangsinApplication.java
@@ -0,0 +1,13 @@
+package com.junfree.jangsin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JangsinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JangsinApplication.class, args);
+	}
+
+}
